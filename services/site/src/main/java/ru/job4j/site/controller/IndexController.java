9268b139e207897284ea5b96410cadba91d8ef87
IvanPavlovets <ivan150287@gmail.com>
2024-04-30 14:22:51 +0700

    4. Микросервисные системы [#338434] - 1. Убрать магические числа. (получаем все interviewsPage по token, page, size) 2. получаю имя из серсиа Profil по id, последнии берем из InterviewDTO, submitterId - это id отправителей интерьвью.

--- services/site/src/main/java/ru/job4j/site/controller/IndexController.java
+++ services/site/src/main/java/ru/job4j/site/controller/IndexController.java
@@ -3,15 +3,20 @@ package ru.job4j.site.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.job4j.site.service.AuthService;
-import ru.job4j.site.service.CategoriesService;
-import ru.job4j.site.service.InterviewsService;
-import ru.job4j.site.service.NotificationService;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.job4j.site.dto.InterviewDTO;
+import ru.job4j.site.dto.ProfileDTO;
+import ru.job4j.site.service.*;
 
 import javax.servlet.http.HttpServletRequest;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static ru.job4j.site.controller.RequestResponseTools.getToken;
 
@@ -22,26 +27,37 @@ public class IndexController {
     private final CategoriesService categoriesService;
     private final InterviewsService interviewsService;
     private final AuthService authService;
+    private final ProfilesService profilesService;
     private final NotificationService notifications;
 
     @GetMapping({"/", "index"})
-    public String getIndexPage(Model model, HttpServletRequest req) throws JsonProcessingException {
+    public String getIndexPage(Model model, HttpServletRequest req,
+                               @RequestParam(required = false, defaultValue = "0") int page,
+                               @RequestParam(required = false, defaultValue = "20") int size) throws JsonProcessingException {
         RequestResponseTools.addAttrBreadcrumbs(model,
                 "Главная", "/"
         );
         try {
             model.addAttribute("categories", categoriesService.getMostPopular());
             var token = getToken(req);
+            Page<InterviewDTO> interviewsPage = interviewsService.getAll(token, page, size);
+            model.addAttribute("new_interviews", interviewsPage);
+            Set<ProfileDTO> userList = interviewsPage.toList().stream()
+                    .map(x -> profilesService.getProfileById(x.getSubmitterId()))
+                    .filter(Optional::isPresent)
+                    .map(Optional::get)
+                    .collect(Collectors.toSet());
+            model.addAttribute("users", userList);
             if (token != null) {
                 var userInfo = authService.userInfo(token);
-                model.addAttribute("userInfo", userInfo);
                 model.addAttribute("userDTO", notifications.findCategoriesByUserId(userInfo.getId()));
+                model.addAttribute("userInfo", userInfo);
                 RequestResponseTools.addAttrCanManage(model, userInfo);
             }
         } catch (Exception e) {
             log.error("Remote application not responding. Error: {}. {}, ", e.getCause(), e.getMessage());
         }
-        model.addAttribute("new_interviews", interviewsService.getByType(1));
         return "index";
     }
+
 }
