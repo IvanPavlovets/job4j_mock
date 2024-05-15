6f9b4ab01be1faa6df502e8a8192b518fbf18585
IvanPavlovets <ivan150287@gmail.com>
2024-05-15 19:04:56 +0700

    RestTemplate [#458496] 3

--- services/site/src/test/java/ru/job4j/site/controller/IndexControllerTest.java
+++ services/site/src/test/java/ru/job4j/site/controller/IndexControllerTest.java
@@ -7,16 +7,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.ui.ConcurrentModel;
 import ru.job4j.site.SiteSrv;
 import ru.job4j.site.domain.Breadcrumb;
 import ru.job4j.site.dto.CategoryDTO;
 import ru.job4j.site.dto.InterviewDTO;
+import ru.job4j.site.dto.ProfileDTO;
 import ru.job4j.site.dto.TopicDTO;
 import ru.job4j.site.service.*;
 
+import java.util.Calendar;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.when;
@@ -78,18 +84,31 @@ class IndexControllerTest {
         topicDTO2.setName("topic2");
         var cat1 = new CategoryDTO(1, "name1");
         var cat2 = new CategoryDTO(2, "name2");
+        var token = "1410";
+        var id = 1;
+        var profile = new ProfileDTO(id, "username", "experience", 1,
+                Calendar.getInstance(), Calendar.getInstance());
         var listCat = List.of(cat1, cat2);
-        var firstInterview = new InterviewDTO(1, 1, 1, 1,
-                "interview1", "description1", "contact1",
-                "30.02.2024", "09.10.2023", 1);
-        var secondInterview = new InterviewDTO(2, 1, 1, 2,
-                "interview2", "description2", "contact2",
-                "30.02.2024", "09.10.2023", 1);
-        var listInterviews = List.of(firstInterview, secondInterview);
+        List<InterviewDTO> interviews = IntStream.range(0, 4).mapToObj(i -> {
+            var interview = new InterviewDTO();
+            interview.setId(i);
+            interview.setMode(1);
+            interview.setStatus(1);
+            interview.setSubmitterId(1);
+            interview.setTitle(String.format("Interview_%d", i));
+            interview.setAdditional("Some text");
+            interview.setContactBy("Some contact");
+            interview.setApproximateDate("30.02.2024");
+            interview.setCreateDate("06.10.2023");
+            return interview;
+        }).toList();
+        Page<InterviewDTO> page = new PageImpl<>(interviews);
         when(topicsService.getByCategory(cat1.getId())).thenReturn(List.of(topicDTO1));
         when(topicsService.getByCategory(cat2.getId())).thenReturn(List.of(topicDTO2));
         when(categoriesService.getMostPopular()).thenReturn(listCat);
-        when(interviewsService.getByType(1)).thenReturn(listInterviews);
+        //when(interviewsService.getByType(1)).thenReturn(listInterviews);
+        when(interviewsService.getAll(token, 0, 20)).thenReturn(page);
+        when(profilesService.getProfileById(id)).thenReturn(Optional.of(profile));
         var listBread = List.of(new Breadcrumb("Главная", "/"));
         var model = new ConcurrentModel();
         var view = indexController.getIndexPage(model, null, 0, 20);
@@ -97,11 +116,12 @@ class IndexControllerTest {
         var actualBreadCrumbs = model.getAttribute("breadcrumbs");
         var actualUserInfo = model.getAttribute("userInfo");
         var actualInterviews = model.getAttribute("new_interviews");
-
+        var actualUsers = model.getAttribute("users");
         assertThat(view).isEqualTo("index");
         assertThat(actualCategories).usingRecursiveComparison().isEqualTo(listCat);
         assertThat(actualBreadCrumbs).usingRecursiveComparison().isEqualTo(listBread);
         assertThat(actualUserInfo).isNull();
-        assertThat(actualInterviews).usingRecursiveComparison().isEqualTo(listInterviews);
+        assertThat(actualInterviews).isNull();
+        assertThat(actualUsers).isNull();
     }
 }
