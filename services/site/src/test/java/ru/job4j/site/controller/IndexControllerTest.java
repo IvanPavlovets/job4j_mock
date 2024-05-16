5bdfd749494e5916e569e52491e18ea197b5b1e3
IvanPavlovets <ivan150287@gmail.com>
2024-05-16 13:40:35 +0700

    RestTemplate [#458496] 6

--- services/site/src/test/java/ru/job4j/site/controller/IndexControllerTest.java
+++ services/site/src/test/java/ru/job4j/site/controller/IndexControllerTest.java
@@ -13,23 +13,20 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.ui.ConcurrentModel;
 import ru.job4j.site.SiteSrv;
 import ru.job4j.site.domain.Breadcrumb;
-import ru.job4j.site.dto.CategoryDTO;
-import ru.job4j.site.dto.InterviewDTO;
-import ru.job4j.site.dto.ProfileDTO;
-import ru.job4j.site.dto.TopicDTO;
+import ru.job4j.site.dto.*;
 import ru.job4j.site.service.*;
 
 import java.util.Calendar;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.IntStream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 /**
@@ -84,19 +81,44 @@ class IndexControllerTest {
         topicDTO2.setName("topic2");
         var cat1 = new CategoryDTO(1, "name1");
         var cat2 = new CategoryDTO(2, "name2");
+        var token = "1410";
+        var id = 1;
+        var profile = new ProfileDTO(id, "username", "experience", 1,
+                Calendar.getInstance(), Calendar.getInstance());
         var listCat = List.of(cat1, cat2);
+        List<InterviewDTO> interviews = IntStream.range(0, 4).mapToObj(i -> {
+            var interview = new InterviewDTO();
+            interview.setId(i);
+            interview.setMode(1);
+            interview.setStatus(1);
+            interview.setSubmitterId(1);
+            interview.setTitle(String.format("Interview_%d", i));
+            interview.setAdditional("Some text");
+            interview.setContactBy("Some contact");
+            interview.setApproximateDate("30.02.2024");
+            interview.setCreateDate("06.10.2023");
+            return interview;
+        }).toList();
+        Page<InterviewDTO> page = new PageImpl<>(interviews);
+        Set<ProfileDTO> userList = Set.of(profile);
         when(topicsService.getByCategory(cat1.getId())).thenReturn(List.of(topicDTO1));
         when(topicsService.getByCategory(cat2.getId())).thenReturn(List.of(topicDTO2));
         when(categoriesService.getMostPopular()).thenReturn(listCat);
+        when(interviewsService.getAll(token, 1, 5)).thenReturn(page);
+        when(profilesService.getProfileById(id)).thenReturn(Optional.of(profile));
         var listBread = List.of(new Breadcrumb("Главная", "/"));
         var model = new ConcurrentModel();
-        var view = indexController.getIndexPage(model, null, 0, 20);
+        var view = indexController.getIndexPage(model, null, 1, 5);
         var actualCategories = model.getAttribute("categories");
         var actualBreadCrumbs = model.getAttribute("breadcrumbs");
         var actualUserInfo = model.getAttribute("userInfo");
+        var actualInterviews = model.getAttribute("new_interviews");
+        var actualUsers = model.getAttribute("users");
         assertThat(view).isEqualTo("index");
         assertThat(actualCategories).usingRecursiveComparison().isEqualTo(listCat);
         assertThat(actualBreadCrumbs).usingRecursiveComparison().isEqualTo(listBread);
         assertThat(actualUserInfo).isNull();
+        assertThat(actualInterviews).isNull();
+        assertThat(actualUsers).isNull();
     }
 }
